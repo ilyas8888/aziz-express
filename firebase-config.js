54f668e838921085fabebe12248c8d8a2ce88ad9
ilyas8888 <ilyassboulouiz@outlook.com>
2026-04-20 16:46:13 +0100

    Aziz Express - initial

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+    const firebaseConfig = {
+        apiKey: "AIzaSyD-gvOCze-SVwzjBvLBOSh9ZkpFQ5dJHEE",
+        authDomain: "rent-4db84.firebaseapp.com",
+        databaseURL: "https://rent-4db84-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "rent-4db84",
+        storageBucket: "rent-4db84.firebasestorage.app",
+        messagingSenderId: "606626161593", 
+        appId: "1:606626161593:web:ed5a11466a38badd524c68",
+        measurementId: "G-DZGNZ46LYJ" 
+    };
+firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
